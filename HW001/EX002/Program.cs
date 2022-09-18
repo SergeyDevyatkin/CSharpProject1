0f1d182774bfd66e73371db40a0967d4d9fa6b30
Sergey Devyatkin <lut17@mail.ru>
2022-09-18 14:32:48 +0300

    Задание2

---
+++ HW001/EX002/Program.cs
@@ -0,0 +1,15 @@
+﻿var a = 2 ;
+var b = 3 ;
+var c  = 7;
+if (c > a && a > b)
+{
+    Console.WriteLine("a=max");
+}
+if ( c < b && b > a)
+{
+    Console.WriteLine("b=max");
+}
+if (a < c && c > b)
+{
+    Console.WriteLine("c=max");
+}
