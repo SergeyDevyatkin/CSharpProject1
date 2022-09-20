90ca0e5679fb2734f28b18a46f6eecc9ea629973
Sergey Devyatkin <lut17@mail.ru>
2022-09-21 00:35:19 +0300

    Задача 3 д/з 2

---
+++ HW002/EX003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите цифру, обозначающую день недели: "); //понедельник - 1, вторник - 2, среда -3, четверг - 4, пятница - 5, суббота - 6, воскресенье - 7.
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+void DayOfTheWeek (int numberDay) {
+  if (numberDay == 6 || numberDay == 7) {
+  Console.WriteLine(" выходной день ");
+  }
+  else Console.WriteLine(" рабочий день ");
+}
+DayOfTheWeek(numberDay);
