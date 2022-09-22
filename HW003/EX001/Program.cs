8d2bb22e2bcf6957550f6754e5fbfaef1c7abdb4
Sergey Devyatkin <lut17@mail.ru>
2022-09-22 22:59:11 +0300

    Задача 1 д/з 3

---
+++ HW003/EX001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void Number(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($" число: {number} - палиндром.");
+  }
+  else Console.WriteLine($" число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  Number(number);
+}
+else Console.WriteLine($"Введиnt правильное число");
+
