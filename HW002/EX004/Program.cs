9137c8543bed68cfbc5168b91389d4aaa34fe228
Sergey Devyatkin <lut17@mail.ru>
2022-09-22 22:38:34 +0300

    Задача 4 д/з 2

---
+++ HW002/EX004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int[] array =  {10, 20, 30, 40, 50, 60, 70};
+int n = array.Length;
+int sum = 0;
+for (int i = 0; i < n; i++)
+sum = sum + array[i];
+int sr = sum / array.Length ;
+Console.WriteLine("Среднее арифметическое");
+Console.WriteLine(sr);
+
+
