26c79e30aa61b9a25c3f13ecf8582ce2b5db677a
Sergey Devyatkin <lut17@mail.ru>
2022-10-01 22:35:15 +0300

    Задача2

---
+++ HW005/Ex002/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int size = 10;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+void Search(int[] array)
+{
+    int sum = 0;
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (i % 2 > 0) sum += array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"The sum of numbers standing in an odd position  {sum} ");
+    Console.WriteLine();
+}
+
+Search(FillArray(size));
+
