86f0e067ce330a80f0db17b0f5e09318f6f9efdd
Sergey Devyatkin <lut17@mail.ru>
2022-10-01 22:50:49 +0300

    Задача3

---
+++ HW005/Ex003/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+int size = new Random().Next(1, 11);
+
+double[] FillArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    Console.WriteLine($"min {arr.Min()}, max {arr.Max()}");
+    Console.WriteLine($"max - min = {difference}");
+   Console.WriteLine();
+}
+
+PrintDiffMinMax(FillArray(size));
