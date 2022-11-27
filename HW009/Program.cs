90286f668cb3408f8876441b6b277aaafd60d03d
Sergey Devyatkin <lut17@mail.ru>
2022-11-27 16:49:10 +0300

    Добавил метод вывода результата задачи

--- HW009/Program.cs
+++ HW009/Program.cs
@@ -1,4 +1,4 @@
-﻿    // Задача: Написать программу, которая из сформированного массива 
+﻿// Задача: Написать программу, которая из сформированного массива 
 // строк содержащих символы и числа создать массив из строк,
 //  в который попадут только символы, не являющиеця цифрами.
 // Входные данные можно получить путём ввода с клавиатуры,
@@ -8,13 +8,30 @@ string words = " ";
 int size = 20;
 char[] letter = ("ABCDEFGHJKLMNOPRSTQUVWXYZ1234567890").ToCharArray();
 
-char[] PrintArray(char[] arr)
+char[] NewArray(char[] arr)
 {
     for (int i = 0; i < size; i++)
     {
         int c = new Random().Next(0, letter.Length - 1);
         words += letter[c];
     }
-    Console.Write(words);
+    Console.Write($"Исходный массив - {words} ");
     return arr;
 }
+void PrintResult(char[] arr)
+{
+    string result = string.Empty;
+    foreach(var item in words)
+{
+if(!char.IsDigit(item))
+result+= item;
+}
+    Console.WriteLine($"Новый массив - {result}");
+}
+
+NewArray(letter);
+Console.WriteLine();
+PrintResult(letter);
+
+
+
